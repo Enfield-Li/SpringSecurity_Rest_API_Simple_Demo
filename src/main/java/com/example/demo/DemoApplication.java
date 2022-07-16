f03df1aeeacb38ecefdff3cac49cdfdf6d9cfa62
Enfield-Li <1076462153@qq.com>
2022-07-16 15:55:31 +0800

    Final settings

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 
   public static void main(String[] args) throws IOException {
     SpringApplication.run(DemoApplication.class, args);
-    // openSwaggerUI();
+    openSwaggerUI();
   }
 
   private static void openSwaggerUI() throws IOException {
